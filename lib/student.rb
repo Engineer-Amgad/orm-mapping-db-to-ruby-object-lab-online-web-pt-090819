bd74ced41dfa9341367f4fe0eee2ce6e5a051e11
Amgad Malik <amgadmalik@yahoo.com>
2019-12-07 01:50:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,4 +77,19 @@ class Student
         #self.new_from_db(row)
       #end.first 
   end
+  
+  def self.all_students_in_grade_9
+    # This method should return an array of all the students in grade 9
+    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+      SQL
+      
+      DB[:conn].execute(sql) #.map do |row|
+        #self.new_from_db(row)
+      #end.first 
+  end
+  
 end
