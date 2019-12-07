30a48e79661127552d2a7f31621f262e2fe42a5e
Amgad Malik <amgadmalik@yahoo.com>
2019-12-07 02:26:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,11 +102,20 @@ class Student
       DB[:conn].execute(sql, x).map do |row|
         self.new_from_db(row)
       end
-      
-    
-    # sql = "SELECT * FROM students WHERE grade = 10 LIMIT ?"
-    # DB[:conn].execute(sql, x)
-
   end
+  
+  def self.first_X_students_in_grade_10(x)
+    # This method should return an array of exactly X number of students
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+      SQL
+      
+      DB[:conn].execute(sql, x).map do |row|
+        self.new_from_db(row)
+      end
+  
   
 end
